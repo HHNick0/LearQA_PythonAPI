32a269407cb3d44ffc6bb868df03c0605a3f3068
n.vasin <nsvasinn@gmail.com>
2023-03-09 16:17:40 +0300

    1

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,45 +1,23 @@
-from datetime import datetime
-
-import requests
-
+from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
-
 from lib.assertions import Assertions
 
 
 class TestUserRegister(BaseCase):
 
-    def setup_method(self):
-        base_part = "learnqa"
-        domain = "example.com"
-        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
-        self.email = f"{base_part}{random_part}@{domain}"
-
     def test_create_user_sucsessfuly(self):
-        data = {
-            "password": "123",
-            "username": "learnqa",
-            "firstName": "learnqa",
-            "lastName": "learnqa",
-            "email": self.email
-        }
+        data = self.prepare_registration_data()
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 200)
-        Assertions.asser_json_has_key(response, "id")
+        Assertions.assert_json_has_key(response, "id")
 
     def test_create_user_with_existing_email(self):
         email = "vinkotov@example.com"
-        data = {
-            "password": "123",
-            "username": "learnqa",
-            "firstName": "learnqa",
-            "lastName": "learnqa",
-            "email": email
-        }
+        data = self.prepare_registration_data(email)
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
